118446406d09a290442d7dc05b2bb2c59ef7e47b
Nicole Steadman <nsteadman@gmail.com>
2022-08-13 19:23:36 -0700

    added Home, Login, Navbar, and Signup components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,22 @@
 import { useState, useEffect } from "react";
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   
 
   return (
     <Router>
-        <Routes>
+      <Navbar />
+        {/* <Routes> */}
           <Route path="/" element={<Home/>}/>
           <Route path="/signup" element={<Signup/>}/>
           <Route path="/login" element={<Login/>}/>
-        </Routes>
+        {/* </Routes> */}
     </Router>
   );
 }
